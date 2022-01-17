d1dcb0c8bc755bdca69b31ea7c5f788a59778ff4
Jarred Cianciulli <jcianci1@gmail.com>
2022-01-17 17:33:29 -0500

    logout and signup fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,14 +52,20 @@ const createSendToken = (user, statusCode, req, res) => {
 // });
 
 exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
   // 3) If everything ok, send token to client
-
-  const newUser = await User.create(req.body);
-
-  const url = `${req.protocol}://${req.get('host')}/me`;
-  console.log(url);
-  await new Email(newUser, url).sendWelcome();
-
   createSendToken(user, 200, req, res);
 });
 
