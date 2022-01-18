9b9ae286270a14dc6e135f1e6e5001df4ada6008
Jarred Cianciulli <jcianci1@gmail.com>
2022-01-18 02:15:04 -0500

    footer media queries

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9038,7 +9038,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60075" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58922" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
